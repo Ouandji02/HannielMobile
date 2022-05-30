17427f2a36256e5f5af02bcdf2b0b1aeba7524ca
nguewouo mabkop manick <nguewouom@gmail.com>
2022-05-31 01:11:21 +0200

    update

--- lib/screens/PatientDetails.dart
+++ lib/screens/PatientDetails.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 import '../CONSTANTS/color.dart';
+import '../CONSTANTS/style.dart';
 
 class PatientDetails extends StatefulWidget {
   const PatientDetails({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class PatientDetails extends StatefulWidget {
 }
 class _PatientDetails extends State<PatientDetails> {
   @override
+  String dropdownValue = "Male";
   Widget build(BuildContext context) {
 
     return Scaffold(
@@ -32,20 +34,9 @@ class _PatientDetails extends State<PatientDetails> {
         ),
         centerTitle: true,
         title: Text(
-          "Prescription Detail",
+          "Patient Details",
           style: TextStyle(color: HexColor(COLOR_TITLE)),
         ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              null;
-            },
-            child: Icon(
-              Icons.search,
-              color: HexColor(COLOR_PRIMARY),
-            ),
-          )
-        ],
       ),
       body: Container(
           decoration: BoxDecoration(color: Colors.white),
@@ -53,6 +44,86 @@ class _PatientDetails extends State<PatientDetails> {
             children: [
               Container(
                   decoration: BoxDecoration(color: Colors.white),
+                child: Card(
+                  elevation: 0,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: Icon(Icons.person),
+                    ),
+                    title: Text("Simo Larissa", style: TextStyle(color: HexColor(COLOR_TITLE), fontWeight: FontWeight.bold,),),
+                    subtitle: Text("Family Doctor, Cardiologist", style: TextStyle(color: HexColor(COLOR_SUBTITLE), fontSize: 12),),
+                    ),
+                  ),
+                ),
+              Container(
+                child: Card(
+                  elevation: 0,
+                  child: ListTile(
+                    title: Text("Purpose of visit", style: TextStyle(fontSize: 12),),
+                    subtitle: Text("Consultation", style: TextStyle(color: HexColor(COLOR_TITLE), fontWeight: FontWeight.bold, fontSize: 15),),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.grey),
+                    top: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+              Container(
+                child: Card(
+                  elevation: 0,
+                  child: ListTile(
+                    title: Text("Date and time", style: TextStyle(fontSize: 12),),
+                    subtitle: Text("Tomorrow, 8:30 AM", style: TextStyle(color: HexColor(COLOR_TITLE), fontWeight: FontWeight.bold, fontSize: 15),),
+                  ),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      child: Text("This appointment for:", style: TextStyle(fontWeight: FontWeight.bold, color: HexColor(COLOR_TITLE)),),
+                    ),
+                    Container(
+                      child: Card(
+                        elevation: 0,
+                        child: Container(
+                          alignment: AlignmentDirectional.centerStart,
+                          child:Container(
+                                child: DropdownButton<String>(
+                                  value: dropdownValue,
+                                  icon: const Icon(Icons.arrow_downward),
+                                  elevation: 16,
+                                  style: const TextStyle(
+                                      color: Colors.deepPurple),
+                                  underline: Container(
+                                    height: 2,
+                                    color: Colors.deepPurpleAccent,
+                                  ),
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      dropdownValue = newValue!;
+                                    });
+                                  },
+                                  items: <String>['Male', 'Female']
+                                      .map<DropdownMenuItem<String>>(
+                                          (String value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Text(value),
+                                        );
+                                      }).toList(),
+                                  isExpanded: true,
+                                ),
+                          ),
+                        ),
+                      ),
+                      ),
+                  ],
+                ),
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 20),
               )
             ],
           )
